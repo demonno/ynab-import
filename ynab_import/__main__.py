89e4601951975a3937424a1d2387098c4c2d8f0b
Demur Nodia <demur.nodia@gmail.com>
2022-04-03 11:52:02 +0300

    Add QA workflows (#11)
    * Add safety step
    
    * Add safety job
    
    * Fix yml
    
    * Poetry run
    
    * Add safety
    
    * Ad dlinter jobs
    
    * Remove args isort
    
    * poetry add isort and black
    
    * Install dependencies
    
    * Black format
    
    * Upgrade black
    
    * Format

--- ynab_import/__main__.py
+++ ynab_import/__main__.py
@@ -2,9 +2,9 @@
 """
 import typer
 
-from ynab_import.setup import create_reader, create_writer
 from ynab_import.common.settings import ReaderKind, WriterKind
 from ynab_import.core import import_transactions
+from ynab_import.setup import create_reader, create_writer
 
 app = typer.Typer(add_completion=False)
 
