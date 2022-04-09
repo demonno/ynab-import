cdef199d5a8b5aaf3efdb0a46048d0b0537159ce
demonno <demur.nodia@gmail.com>
2022-04-09 08:36:00 +0300

    Implement dummy CLI based on config file

--- ynab_import/__main__.py
+++ ynab_import/__main__.py
@@ -1,26 +1,71 @@
 """Invoke as `ynab-import' or `python -m ynab_import'.
 """
+from pydantic import ValidationError
 import typer
+from pprint import pprint
 
-from ynab_import.common.settings import ReaderKind, WriterKind
+from pathlib import Path
+import json
+from ynab_import.common import ReaderKind, WriterKind, Settings
 from ynab_import.core import import_transactions
 from ynab_import.setup import create_reader, create_writer
 
-app = typer.Typer(add_completion=False)
+app = typer.Typer()
+state = {"verbose": False}
+
+
+@app.callback()
+def main(
+    config: Path = typer.Option(
+        ...,
+        exists=True,
+        file_okay=True,
+        dir_okay=False,
+        writable=False,
+        readable=True,
+        resolve_path=True
+    ),
+    verbose: bool = False,
+):
+    """
+    CLI for managing read/transform/import operatoins
+    """
+    if verbose:
+        state["verbose"] = True
+    try:
+        state["config"] = Settings(config)
+    except ValidationError as exc:
+        typer.echo("Invalid configuration")
+        typer.echo(exc)
+
 
 
 @app.command()
+def check():
+    """
+    Print config to be used for importing
+    """
+    settings = state["config"]
+    pprint(settings.dict())
+
+
+
+@app.command()
+def read():
+    """
+    Print transactions from specified source
+    """
+    settings = state["config"]
+    typer.echo(f"Reading transactions with conifg {settings.dict()}")
+
+
+@app.command(name="import")
 def cli(
-    reader: ReaderKind = ReaderKind.swedbank_csv,
-    writer: WriterKind = WriterKind.ynab_api,
+    reader: ReaderKind,
+    writer: WriterKind,
 ):
-    reader = create_reader(reader)
-    writer = create_writer(writer)
-    count = import_transactions(read=reader, write=writer)
-    message = f"Transaction(s) imported: N {count} " + typer.style(
-        "good", fg=typer.colors.GREEN, bold=True
-    )
-    typer.echo(message)
-
-
-app()
+    """
+    Read/Import transactoins from source to destination
+    """
+    settings = state["config"]
+    typer.echo(f"Import transactions with conifg {settings.dict()}")
